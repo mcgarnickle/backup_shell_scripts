7a9442aad911235f99d7b2bf0e9f1e990eb45c87
alan necula <mcbain10@hotmail.com>
2014-03-18 22:07:53 -0400

    skip config files

--- dry_backup.sh
+++ dry_backup.sh
@@ -1,11 +1,14 @@
-## DRY BACKUP ##
+#################
+## DRY BACKUP  ##
+#################
 
 srcDir=$1
 destDir=$2
 
-#local docs
-sudo rsync -navh ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text-2/ | grep -v 'uptodate'
+if [[ ! -z $3 ]]; then
+  #local docs
+  sudo rsync -navh ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text-2/ | grep -v 'uptodate'
+fi
 
 #backup source in full
 sudo rsync -navh "$srcDir" "$destDir" --delete --exclude='*~' --exclude=/lost+found* --exclude=/.Trash* --exclude=/autorun* --exclude=/\$RECYCLE.BIN* --exclude=/RECYCLER* --exclude=/System\ Volume\ Information* --exclude=/wubildr | grep -v 'uptodate'
-
