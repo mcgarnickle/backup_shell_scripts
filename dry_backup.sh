f79bc00e2c15f0b20c753e44e44af2389c60bb0c
alan necula <mcbain10@hotmail.com>
2014-04-27 23:19:53 -0400

    added more config files

--- dry_backup.sh
+++ dry_backup.sh
@@ -1,13 +1,24 @@
 #################
-## DRY BACKUP  ##
+## dry backup  ##
 #################
 
 srcDir=$1
 destDir=$2
 
 if [[ ! -z $3 ]]; then
-  #local docs
+  #sublime text
   sudo rsync -navh ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text-2/ | grep -v 'uptodate'
+  #squeezebox
+  sudo rsync -navh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ | grep -v 'uptodate'
+  #firefox
+  sudo rsync -navh /home/alan/.mozilla/firefox/w2xnqmo9.default/sessionstore.* "$srcDir"config/firefox/ | grep -v 'uptodate'
+  sudo rsync -navh /home/alan/.mozilla/firefox/w2xnqmo9.default/sessionbackups/ "$srcDir"config/firefox/ | grep -v 'uptodate'
+  #gnome file associations
+  sudo rsync -navh /etc/gnome/defaults.list "$srcDir"config/file_associations/ | grep -v 'uptodate'
+  #files bookmarks
+  sudo rsync -navh ~/.config/gtk-3.0/bookmarks "$srcDir"config/bookmarks/ | grep -v 'uptodate'
+  #bash
+  sudo rsync -navh ~/.bash* "$srcDir"config/bash/ | grep -v 'uptodate'
 fi
 
 #backup source in full
