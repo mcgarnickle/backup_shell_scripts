272b1f5697e6bd8b600c6185cd5cc15c09eeeed1
mcgarnickle <mcbain10@hotmail.com>
2014-09-05 20:33:59 -0400

    modifies some config file paths

--- dry_backup.sh
+++ dry_backup.sh
@@ -7,13 +7,13 @@ destDir=$2
 
 if [[ ! -z $3 ]]; then
   #sublime text
-  rsync -navh ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text-2/ | grep -v 'uptodate'
+  rsync -navh ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text/ | grep -v 'uptodate'
   #squeezebox
   rsync -navh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ | grep -v 'uptodate'
   #firefox
-  rsync -navh /home/alan/.mozilla/firefox/w2xnqmo9.default/sessionstore.* "$srcDir"config/firefox/ | grep -v 'uptodate'
-  rsync -navh /home/alan/.mozilla/firefox/w2xnqmo9.default/sessionbackups/ "$srcDir"config/firefox/sessionbackups/ | grep -v 'uptodate'
-  rsync -navh /home/alan/.mozilla/firefox/w2xnqmo9.default/sessions/ "$srcDir"config/firefox/sessions/ | grep -v 'uptodate'
+  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionstore.* "$srcDir"config/firefox/ | grep -v 'uptodate'
+  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionbackups/ "$srcDir"config/firefox/sessionbackups/ | grep -v 'uptodate'
+  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessions/ "$srcDir"config/firefox/sessions/ | grep -v 'uptodate'
   #gnome file associations
   rsync -navh /etc/gnome/defaults.list "$srcDir"config/file_associations/ | grep -v 'uptodate'
   #files bookmarks
