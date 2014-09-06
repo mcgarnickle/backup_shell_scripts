74f97b8b80c6404956cc5e374e0df1ed00c0378a
mcgarnickle <mcbain10@hotmail.com>
2014-09-06 03:15:33 -0400

    exlude .git on configs

--- backup.sh
+++ backup.sh
@@ -5,21 +5,22 @@
 srcDir=$1
 destDir=$2
 
+#copy config files to source first
 if [[ ! -z $3 ]]; then
   #sublime text
-  sudo rsync -avh ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text/ | grep -v 'uptodate'
+  sudo rsync -avh ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text/ --exclude=.git* | grep -v 'uptodate'
   #squeezebox
-  sudo rsync -avh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ | grep -v 'uptodate'
+  sudo rsync -avh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ --exclude=.git* | grep -v 'uptodate'
   #firefox
-  sudo rsync -avh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionstore.* "$srcDir"config/firefox/ | grep -v 'uptodate'
-  sudo rsync -avh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionbackups/ "$srcDir"config/firefox/ | grep -v 'uptodate'
-  sudo rsync -avh /home/alan/.mozilla/firefox/wmc89qgm.default/sessions/ "$srcDir"config/firefox/sessions/ | grep -v 'uptodate'
+  sudo rsync -avh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionstore.* "$srcDir"config/firefox/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -avh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionbackups/ "$srcDir"config/firefox/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -avh /home/alan/.mozilla/firefox/wmc89qgm.default/sessions/ "$srcDir"config/firefox/sessions/ --exclude=.git* | grep -v 'uptodate'
   #gnome file associations
-  sudo rsync -avh /etc/gnome/defaults.list "$srcDir"config/file_associations/ | grep -v 'uptodate'
+  sudo rsync -avh /etc/gnome/defaults.list "$srcDir"config/file_associations/ --exclude=.git* | grep -v 'uptodate'
   #files bookmarks
-  sudo rsync -avh ~/.config/gtk-3.0/bookmarks "$srcDir"config/bookmarks/ | grep -v 'uptodate'
+  sudo rsync -avh ~/.config/gtk-3.0/bookmarks "$srcDir"config/bookmarks/ --exclude=.git* | grep -v 'uptodate'
   #bash
-  sudo rsync -avh ~/.bash* "$srcDir"config/bash/ | grep -v 'uptodate'
+  sudo rsync -avh ~/.bash* "$srcDir"config/bash/ --exclude=.git* | grep -v 'uptodate'
 fi
 
 #backup source in full
--- dry_backup.sh
+++ dry_backup.sh
@@ -7,19 +7,19 @@ destDir=$2
 
 if [[ ! -z $3 ]]; then
   #sublime text
-  rsync -navh ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text/ | grep -v 'uptodate'
+  rsync -navh ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text/ --exclude=.git* | grep -v 'uptodate'
   #squeezebox
-  rsync -navh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ | grep -v 'uptodate'
+  rsync -navh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ --exclude=.git* | grep -v 'uptodate'
   #firefox
-  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionstore.* "$srcDir"config/firefox/ | grep -v 'uptodate'
-  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionbackups/ "$srcDir"config/firefox/sessionbackups/ | grep -v 'uptodate'
-  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessions/ "$srcDir"config/firefox/sessions/ | grep -v 'uptodate'
+  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionstore.* "$srcDir"config/firefox/ --exclude=.git* | grep -v 'uptodate'
+  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessionbackups/ "$srcDir"config/firefox/sessionbackups/ --exclude=.git* | grep -v 'uptodate'
+  rsync -navh /home/alan/.mozilla/firefox/wmc89qgm.default/sessions/ "$srcDir"config/firefox/sessions/ --exclude=.git* | grep -v 'uptodate'
   #gnome file associations
-  rsync -navh /etc/gnome/defaults.list "$srcDir"config/file_associations/ | grep -v 'uptodate'
+  rsync -navh /etc/gnome/defaults.list "$srcDir"config/file_associations/ --exclude=.git* | grep -v 'uptodate'
   #files bookmarks
-  rsync -navh ~/.config/gtk-3.0/bookmarks "$srcDir"config/bookmarks/ | grep -v 'uptodate'
+  rsync -navh ~/.config/gtk-3.0/bookmarks "$srcDir"config/bookmarks/ --exclude=.git* | grep -v 'uptodate'
   #bash
-  rsync -navh ~/.bash* "$srcDir"config/bash/ | grep -v 'uptodate'
+  rsync -navh ~/.bash* "$srcDir"config/bash/ --exclude=.git* | grep -v 'uptodate'
 fi
 
 #backup source in full
