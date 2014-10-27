b841d37ff98fe9f2abd18458c036962c729ea3ee
mcgarnickle <mcbain10@hotmail.com>
2014-10-26 20:43:11 -0400

    temp switch to dry for testing

--- backup.sh
+++ backup.sh
@@ -6,19 +6,19 @@ configs=$4
 #copy config files to source first
 if [[ $configs == "config" ]]; then
   #sublime text
-  sudo rsync -avh ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -navh ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$srcDir"config/sublime-text/ --exclude=.git* | grep -v 'uptodate'
   #squeezebox
-  sudo rsync -avh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -navh /var/lib/squeezeboxserver/prefs/ "$srcDir"config/squeezebox/ --exclude=.git* | grep -v 'uptodate'
   #firefox
-  sudo rsync -avh ~/.mozilla/firefox/myprofile/sessionstore.* "$srcDir"config/firefox/sessionstore/ --exclude=.git* | grep -v 'uptodate'
-  sudo rsync -avh ~/.mozilla/firefox/myprofile/sessions/ "$srcDir"config/firefox/sessions/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -navh ~/.mozilla/firefox/myprofile/sessionstore.* "$srcDir"config/firefox/sessionstore/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -navh ~/.mozilla/firefox/myprofile/sessions/ "$srcDir"config/firefox/sessions/ --exclude=.git* | grep -v 'uptodate'
   #gnome file associations
-  sudo rsync -avh /etc/gnome/defaults.list "$srcDir"config/file_associations/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -navh /etc/gnome/defaults.list "$srcDir"config/file_associations/ --exclude=.git* | grep -v 'uptodate'
   #files bookmarks
-  sudo rsync -avh ~/.config/gtk-3.0/bookmarks "$srcDir"config/bookmarks/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -navh ~/.config/gtk-3.0/bookmarks "$srcDir"config/bookmarks/ --exclude=.git* | grep -v 'uptodate'
   #bash
-  sudo rsync -avh ~/.bash* "$srcDir"config/bash/ --exclude=.git* | grep -v 'uptodate'
+  sudo rsync -navh ~/.bash* "$srcDir"config/bash/ --exclude=.git* | grep -v 'uptodate'
 fi
 
 #backup source in full
-sudo rsync -avh "$srcDir" "$destDir" --delete --exclude='*~' --exclude=/lost+found* --exclude=/.Trash* --exclude=/autorun* --exclude=/\$RECYCLE.BIN* --exclude=/RECYCLER* --exclude=/System\ Volume\ Information* --exclude=/wubildr --exclude=/Dropbox | grep -v 'uptodate'
+sudo rsync -navh "$srcDir" "$destDir" --delete --exclude='*~' --exclude=/lost+found* --exclude=/.Trash* --exclude=/autorun* --exclude=/\$RECYCLE.BIN* --exclude=/RECYCLER* --exclude=/System\ Volume\ Information* --exclude=/wubildr --exclude=/Dropbox | grep -v 'uptodate'
